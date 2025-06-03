3f6f33b8fe093577c09059ae712f77110ed815f2
Raushann2709 <raushann.shrivastwa@gmail.com>
2025-06-03 19:59:30 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -50,5 +50,4 @@ app.post('/api/submit', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+module.exports = app;
